8e8be55d584a7416932f86b557940cb3089bbd98
Rafi Sarkar <90035566+THE-ASSASSIN0128@users.noreply.github.com>
2022-07-28 15:43:58 +0000

    V1.0.4

--- Interactions/Moderation/clear.js
+++ Interactions/Moderation/clear.js
@@ -1,6 +1,12 @@
 const {
   EmbedBuilder
 } = require("discord.js");
+const {
+  colour,
+  image,
+  url
+} = require(`${process.cwd()}/config.json`);
+const wait = require("timers/promises").setTimeout;
 
 
 module.exports = {
@@ -11,27 +17,59 @@ module.exports = {
     {
       name: "amount",
       description: "Amount of messages to delete.",
-      type: "INTEGER",
+      type: 4,
       required: true
     },
     {
       name: "user",
       description: "The user to delete messages",
-      type: "USER",
-      required: true
-    },
-    {},
-    {},
-    {}
+      type: 6,
+      required: false
+    }
   ],
   execute: async (interaction, client) => {
-    const amount = interaction.options.getInteger(amount);
-    const user = interaction.options.getUser(user);
+    const amount = interaction.options.getInteger("amount");
+    const user = interaction.options.getUser("user");
+    let Response = new EmbedBuilder().setColor(colour.bot);
+    
 
-    if (!amount) return interaction.reply({
+    if (amount > 100 || !amount) return interaction.reply({
       content: "Please enter a valid number between 1-100"
     });
 
-    
+    if (user) {
+      try {
+        const Messages = await interaction.channel.messages.fetch();
+        const i = 0;
+        const filtered = [];
+        (await Messages).filter( (m) => {
+          if (m.user.id === user.id && amount > i) {
+            filtered.push(m);
+            i++;
+          }
+        });
+        const msg = await interaction.channel.bulkDelete(filtered, true);
+        
+        Response.setDescription(`Deleted **__${msg.size}__** Messages of **${user.tag}**.`);
+        
+        interaction.reply({
+          embeds: [Response]
+        });
+        await wait (5000); 
+        interaction.deleteReply();
+      } catch (error) {
+        Response.setDescription(`**Error :**\n${error}.`);
+      };
+    } else {
+      const msg = await interaction.channel.bulkDelete(amount, true);
+        
+      Response.setDescription(`Deleted **__${msg.size}__** Messages.`);
+        
+      interaction.reply({
+        embeds: [Response]
+      });
+      await wait (5000); 
+      interaction.deleteReply();
+    };
   }
 };
--- README.md
+++ README.md
@@ -14,6 +14,6 @@ If you are trying to run the code on replit then you can do the following change
 2. Fork the repl by simply heading to this url (https://replit.com/@THEASSASSIN2/AutoBot)
 3. After forking the repl head to secrects
 4. Add a new secret by adding `Token` as key and `your bot's token` as the value.
-5. After that do `npm install` in the `Shell` It will download all the packages
+5. After that head to `Shell` and run `npm install`. It will download/add all the packages
 6. Now you are ready to start the bot
 7. Enjoy Using My Codes.........
